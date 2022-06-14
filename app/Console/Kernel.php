e36e3686af45131d71eb9eb60a1427cfc3c60d5f
mujtaba <mujtabanaseer.pakistan@gmail.com>
2022-06-14 17:01:37 +0500

    kernal changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('SendEmail')->everyTwoMinutes();
+         $schedule->command('sending:email')->everyTwoMinutes();
     }
 
     /**
